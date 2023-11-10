133b1f59b3dd9ed725a40b547a7983075feabc22
adil <adil@zally.com>
2023-11-10 19:19:49 +0000

    added comments

--- src/download_data.py
+++ src/download_data.py
@@ -5,6 +5,9 @@ import time
 def download_data_from_web():  
     '''
     Download latest historical and crime datasets from web.
+
+
+    returns (Tuple): [pd.DataFrame, pd.DataFrame]. Will return a tuple of two dataframes. The first is the historical data and the second is the recent data
     '''
     historical_df = pd.read_csv('https://data.london.gov.uk/download/recorded_crime_summary/bf244ffa-326b-49cc-a7df-0e3193abe818/MPS%20LSOA%20Level%20Crime%20%28Historical%29.csv')
     recent_df = pd.read_csv('https://data.london.gov.uk/download/recorded_crime_summary/221142dd-f7b2-4209-921e-4de833a82285/MPS%20LSOA%20Level%20Crime%20%28most%20recent%2024%20months%29.csv')
@@ -25,6 +28,8 @@ def download_and_save_data():
     Downloads and saves the data.
 
     Helper functions: save_data() and download_data_from_web()
+
+    Will add a time to the file as a suffix, indicating when this file dwownload was requested.
     '''
     
     request_time = time.strftime('%Y-%m-%d--%H-%M')
